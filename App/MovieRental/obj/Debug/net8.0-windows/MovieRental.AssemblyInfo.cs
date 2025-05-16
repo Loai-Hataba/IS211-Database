8ca6d995df094f3c21c2e1cbe4126ea51cfa5a70
Loai Hataba <LoaiHataba@gmail.com>
2025-05-16 23:17:41 +0300

    Merging

--- App/MovieRental/obj/Debug/net8.0-windows/MovieRental.AssemblyInfo.cs
+++ App/MovieRental/obj/Debug/net8.0-windows/MovieRental.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieRental")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d753c3be52e68a37bc0197da09ecf19780847879")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12ed2540145b98b3d204b8af2324c8dbf891b147")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieRental")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieRental")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
